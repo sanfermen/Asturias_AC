040f5f884b2a20a612c799a8820a0da236f7cf2e
Sandra Fernandez Menendez <sanfermen@Air-de-Sandra.station>
2025-04-03 19:43:11 +0200

    ahora el menu burger se cierra al cambiar de sección

--- js/header_footer.js
+++ js/header_footer.js
@@ -5,26 +5,38 @@ function toggleNav() {
 	nav.classList.toggle("active");
 }
 
+// Función para cerrar el menú
+function closeNav() {
+	let nav = document.querySelector(".nav-links");
+	if (nav.classList.contains("active")) {
+		nav.classList.remove("active");
+	}
+}
+
+// Añadir función al menú burger
 const burger_button = document.getElementById("menu-burger-id");
 burger_button.addEventListener("click", (e) => {
 	toggleNav();
 })
 
+// Funciones para cambiar de sección al pinchar en menú burger, cambiar el color según la sección y cerrar el menú
 const map_burgerMenu = document.getElementById("map_menu");
 map_burgerMenu.addEventListener("click", (e) => {
 	showSection("map-section");
 	changeHeaderColor("map-section");
+	closeNav();
 });
 
 const fav_burgerMenu = document.getElementById("favorites_menu");
 fav_burgerMenu.addEventListener("click", (e) => {
 	showSection("favorites");
 	changeHeaderColor("favorites");
-	// TODO localstorage Tsundoku index.js
+	closeNav();
 });
 
 const home_burgerMenu = document.getElementById("home_menu");
 home_burgerMenu.addEventListener("click", (e) => {
 	showSection("home");
-	changeHeaderColor("home")
+	changeHeaderColor("home");
+	closeNav();
 })
